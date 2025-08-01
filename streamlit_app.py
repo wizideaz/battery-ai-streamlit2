ab59a4959f60a30ec57c9bcd76ab8c70907bd14b
wizideaz <wisdomosaigbovo24@gmail.com>
2025-08-01 17:36:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,10 @@ robot_anim = load_lottie_url("https://assets7.lottiefiles.com/packages/lf20_tll0
 
 # Load model
 try:
-    model = joblib.load("model/soh_predictor_6features.joblib")
-except Exception as e:
-    st.error(f"⚠️ Failed to load model: {e}")
-    st.stop()
+   model = joblib.load("model/soh_predictor_6features.joblib")
+    input_data = np.array([[voltage, current, temperature, load_current, load_voltage, time]])
+    soh = model.predict(input_data)[0]
+    st.success(f"📊 Predicted State of Health: **{soh:.2f}%**")
 
 # --- Header ---
 st_lottie(robot_anim, height=180, key="robot")
